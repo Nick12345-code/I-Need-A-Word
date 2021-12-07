e4379bbf274081cca5cb22c83c20130d5d5bc46d
Nick12345-code <astonishedchocolate@gmail.com>
2021-12-07 11:31:09 +1100

    word list optimisation

--- Assets/Scripts/WordDictionary.cs
+++ Assets/Scripts/WordDictionary.cs
@@ -1,27 +1,20 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WordDictionary : MonoBehaviour
 {
     [SerializeField] int minLetters = 3;
-    [SerializeField] int maxLetters = 11;
     [SerializeField] TextAsset txt;
     string[] dictionary;
     public List<string> words = new List<string>();
     
-
     void Awake()
     {
         LoadPlayerPreferences();
         LoadWordsFromFile();
     }
 
-    void LoadPlayerPreferences()
-    {
-        minLetters = PlayerPrefs.GetInt("MinWordLength");
-        print(minLetters);
-    }
+    void LoadPlayerPreferences() => minLetters = PlayerPrefs.GetInt("MinWordLength");
 
     void LoadWordsFromFile()
     {
@@ -33,9 +26,7 @@ public class WordDictionary : MonoBehaviour
     {
         foreach (string word in dictionary)
         {
-            word.Substring(0, word.Length);
-
-            if (word.Length >= minLetters && word.Length <= maxLetters)
+            if (word.Length >= minLetters)
             {
                 words.Add(word);
             }
--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -1,18 +1,10 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
 
 public class WordGenerator : MonoBehaviour
 {
     [Header("Scripts")]
     [SerializeField] WordDictionary dictionary;
-    [Space]
     public string randomWord;
-    
-    public void GetRandomWordFromDictionary()
-    {
-        randomWord = dictionary.words[Random.Range(0, dictionary.words.Count)];
-        randomWord = randomWord.Substring(0, randomWord.Length - 1);
-        randomWord = randomWord.ToUpper();
-    }
+
+    public void GetRandomWordFromDictionary() => randomWord = dictionary.words[Random.Range(0, dictionary.words.Count)];
 }
