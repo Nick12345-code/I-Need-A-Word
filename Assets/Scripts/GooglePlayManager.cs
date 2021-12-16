5239eba2b0d1f790b8cb68529eeaf51cff19f713
Nick12345-code <astonishedchocolate@gmail.com>
2021-12-16 11:16:56 +1100

    a

--- Assets/Scripts/GooglePlayManager.cs
+++ Assets/Scripts/GooglePlayManager.cs
@@ -44,4 +44,9 @@ public class GooglePlayManager : MonoBehaviour
             }
         });
     }
+
+    public void ShowLeaderBoard()
+    {
+        Social.ShowLeaderboardUI (); // Show all leaderboard
+    }
 }
--- Assets/Scripts/Saving/GameData.cs
+++ Assets/Scripts/Saving/GameData.cs
@@ -2,11 +2,9 @@
 public class GameData 
 {
     public int iqTotal;
-    public int timeTotal;
 
     public GameData()
     {
         iqTotal = 0;
-        timeTotal = 120;
     }
 }
