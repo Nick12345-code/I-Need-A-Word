c98f278cae9d8ddc1d79f3affa7022d96cc1c560
Nick12345-code <astonishedchocolate@gmail.com>
2021-12-30 13:25:06 +1100

    a

--- Assets/Scripts/General/SceneChange.cs
+++ Assets/Scripts/General/SceneChange.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneChange : MonoBehaviour
 {
+    [SerializeField] AudioSource musicAudioSource;
+    [SerializeField] float secondsToFadeOut = 1.0f;
     [SerializeField] Animator animator;
     string sceneName;
 
@@ -16,5 +19,20 @@ public class SceneChange : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void FadeAudio()
+    {
+        StartCoroutine(FadeAudioCoroutine());
+    }
+
+    IEnumerator FadeAudioCoroutine()
+    {
+        while (musicAudioSource.volume > 0.01f)
+        {
+            musicAudioSource.volume -= Time.deltaTime / secondsToFadeOut;
+            yield return null;
+        }
+        musicAudioSource.volume = 0.0f;
     }
 }
